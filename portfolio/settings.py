bac1ef485fb983070ad7cd74b789684da453c026
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2025-03-21 23:35:03 +0000

    Create basic theme with tailwind.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,6 +45,12 @@ ALLOWED_HOSTS = [
     "35.181.96.100",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://www.gurdeepkumar.com",
+]
+
+TAILWIND_APP_NAME = "theme"
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,6 +60,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -64,6 +74,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "portfolio.urls"
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
