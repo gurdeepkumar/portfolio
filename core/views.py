9ef42e7109e2672635f6bb7b2647dc1d0a66a2ea
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2025-07-18 01:00:01 +0100

    Revamp the template. CV download added.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404
+import os
+from django.conf import settings
+
+
+def download_file(request, filename):
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    if os.path.exists(file_path):
+        response = FileResponse(
+            open(file_path, "rb"), as_attachment=True, filename=filename
+        )
+        return response
+    else:
+        raise Http404("File does not exist")
 
 
 def home(request):
